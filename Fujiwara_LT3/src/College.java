18ffe3aa8b637d589dcea5f188e67805e8798886
kristinafujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2022-10-13 20:50:03 +0800

    Add files via upload

---
+++ Fujiwara_LT3/src/College.java
@@ -0,0 +1,8 @@
+class College{
+    public void BSIT(){
+        System.out.println("ITP203");
+    }
+    public void LaSalle() {
+        System.out.println("Object Oriented Programming");
+    }
+}
